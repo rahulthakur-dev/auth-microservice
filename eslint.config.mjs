5901b667a98204083a6b7b27fa41eee7c9b82a96
rahul-thakur-01 <rahulthakur.developer@gmail.com>
2025-03-10 10:58:54 +0530

    config eslint for ts

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: [
+            'node_modules',
+            'dist',
+            'eslint.config.mjs',
+            'jest.config.js',
+            'scripts/generateKey.mjs',
+            'scripts/convertPemToJwk.mjs',
+            'tests',
+            '**/*.spec.ts',
+            '**/*.test.ts',
+            'app.spec.ts',
+            'coverage',
+        ],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRoot: import.meta.dirname,
+            },
+        },
+        rules: {
+            "no-console": ["warn", { "allow": ["warn", "error", "log"] }],
+            'dot-notation': 'error',
+        },
+    },
+)
