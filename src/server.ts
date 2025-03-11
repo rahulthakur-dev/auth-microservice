7a2d270e6871c83468d7c16b04010196801c220e
rahul-thakur-01 <rahulthakur.developer@gmail.com>
2025-03-11 09:12:22 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+import app from './app'
+import { Config } from './config'
+
+const startServer = async () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (err) {
+        console.error('Error starting server:', err)
+        process.exit(1)
+    }
+}
+
+startServer()
